19be9361261f6a689a95bea0eee4596906579aa8
kym9129 <kym9129@naver.com>
2020-12-06 01:48:30 +0900

    과제1번

---
+++ 201204KHassignment/src/emp/model/vo/Employee.java
@@ -0,0 +1,15 @@
+package emp.model.vo;
+
+//기능제공 클래스
+//* 패키지 : emp.model.vo
+//* 클래스명 : Employee
+//* 추가 메소드 : public void printEmployee(){}
+//* 내용 : 자기소개출력 (본인 이름, 나이, 생년월일)
+
+public class Employee {
+	
+	public void printEmployee() {
+		System.out.println("이름: 김영미\n나이:30\n생년월일:1991.02.09");
+	}
+
+}
